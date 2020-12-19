773df66ce7b1c8c4e721fa3c1195117efdff98bd
109598087 <t109598087@ntut.org.tw>
2020-12-19 23:34:16 +0800

    uniform

--- rotation_m_closure_property/uniform_k_arbiter.py
+++ rotation_m_closure_property/uniform_k_arbiter.py
@@ -1,19 +1,25 @@
 from rotation_m_closure_property.is_rotation_m_closure_property import is_rotation_m_closure_property
 from random import sample
 
-N = 9
-k = 2
-U = [i for i in range(N)]
-Q = int(k*N/(k+1)+1)
 
+def uniform_k_arbiter_quorum_system(N, k):
+    U = [i for i in range(N)]
+    Q = int(k * N / (k + 1) + 1)  # 加取下底
+    quorum_system_list = list()
+    for i in range(k + 1):
+        temp_list = sample(U, Q)
+        quorum_system_list.append(temp_list)
+    return quorum_system_list
+
+
+N = 9
+k = 3
 
 true_count = 0
 for i in range(100):  # test 100次
-    uniform_2_arbiter_quorum_system = list()
-    for i in range(k+1):
-        temp_list = sample(U, Q)
-        uniform_2_arbiter_quorum_system.append(temp_list)
-    print(is_rotation_m_closure_property(uniform_2_arbiter_quorum_system, N))
-    if is_rotation_m_closure_property(uniform_2_arbiter_quorum_system, N):
+    uniform_2_arbiter_quorum_system_list = uniform_k_arbiter_quorum_system(N, k)
+    print(uniform_2_arbiter_quorum_system_list)
+    # print(is_rotation_m_closure_property(uniform_2_arbiter_quorum_system_list, N))
+    if is_rotation_m_closure_property(uniform_2_arbiter_quorum_system_list, N):
         true_count += 1
 print(true_count)
