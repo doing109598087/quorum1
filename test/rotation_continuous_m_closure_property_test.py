dd2200929626d2c30f346a7a8daca6df5878e9e4
109598087 <t109598087@ntut.org.tw>
2021-01-22 11:13:41 +0800

    rename test_class

--- test/rotation_continuous_m_closure_property_test.py
+++ test/rotation_continuous_m_closure_property_test.py
@@ -3,7 +3,7 @@ from rotation_continuous_m_closure_property.is_rotation_continuous_m_closure_pro
     get_two_quorum_continuous_overlap, get_all_quorum_continuous_overlap
 
 
-class TestStringMethods(unittest.TestCase):
+class Test_continuous_overlap(unittest.TestCase):
     def test_get_two_quorum_continuous_overlap(self):
         self.assertEqual(get_two_quorum_continuous_overlap([0, 1, 2], [1, 2]), [[1, 2]])
         self.assertEqual(get_two_quorum_continuous_overlap([0, 1, 2], [0, 1, 2]), [[0, 1], [1, 2]])
