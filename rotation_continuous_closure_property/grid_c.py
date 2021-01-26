9129483412134743e5d9246625d1e00b797f0fe6
109598087 <t109598087@ntut.org.tw>
2021-01-26 18:34:27 +0800

    grid_c ok

--- rotation_continuous_closure_property/grid_c.py
+++ rotation_continuous_closure_property/grid_c.py
@@ -6,16 +6,22 @@ from rotation_continuous_closure_property.is_rotation_continuous_closure_propert
 def create_one_continuous_grid_quorum(N, start_row, start_column):
     matrix_np = (np.arange(N)).reshape(int(np.sqrt(N)), int(np.sqrt(N)))  # 建立二維矩陣
     # print(matrix_np)
-    two_row = matrix_np[start_row:start_row + 2, :].ravel()
-    two_column = matrix_np[:, start_column:start_column + 2].ravel()
-    grid_quorm = list(set(list(two_row) + list(two_column)))
-    return grid_quorm
+    if start_row == int(np.sqrt(N) - 1):
+        two_row = list(matrix_np[start_row, :]) + list(matrix_np[0, :])
+    else:
+        two_row = matrix_np[start_row:start_row + 2, :].ravel()
+    if start_column == int(np.sqrt(N) - 1):
+        two_column = list(matrix_np[:, start_column]) + list(matrix_np[:, 0])
+    else:
+        two_column = matrix_np[:, start_column:start_column + 2].ravel()
+    grid_quorum = list(set(list(two_row) + list(two_column)))
+    grid_quorum.sort()
+    return grid_quorum
 
-
-N = 36
-grid_quorum_system = list()
-grid_quorum1 = create_one_continuous_grid_quorum(N, 2, 3)
-grid_quorum2 = create_one_continuous_grid_quorum(N, 0, 0)
-grid_quorum_system.append(grid_quorum1)
-grid_quorum_system.append(grid_quorum2)
-print(is_rotation_continuous_closure_property(grid_quorum_system, N))
+# N = 36
+# grid_quorum_system = list()
+# grid_quorum1 = create_one_continuous_grid_quorum(N, 2, 3)
+# grid_quorum2 = create_one_continuous_grid_quorum(N, 0, 0)
+# grid_quorum_system.append(grid_quorum1)
+# grid_quorum_system.append(grid_quorum2)
+# print(is_rotation_continuous_closure_property(grid_quorum_system, N))
--- test/rotation_continuous_closure_property/grid_c_test.py
+++ test/rotation_continuous_closure_property/grid_c_test.py
@@ -6,5 +6,9 @@ class Test_create_continuous_grid_quorum(unittest.TestCase):
     def test_create_one_continuous_grid_quorum(self):
         self.assertEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 18, 19, 24, 25, 30, 31],
                          create_one_continuous_grid_quorum(36, 0, 0))
-        self.assertEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 18, 19, 24, 25, 30, 31],
+        self.assertEqual([3, 4, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 27, 28, 33, 34],
                          create_one_continuous_grid_quorum(36, 2, 3))
+        self.assertEqual([0, 1, 2, 3, 4, 5, 6, 7, 12, 13, 18, 19, 24, 25, 30, 31, 32, 33, 34, 35],
+                         create_one_continuous_grid_quorum(36, 5, 0))
+        self.assertEqual([0, 1, 2, 3, 4, 5, 6, 11, 12, 17, 18, 23, 24, 29, 30, 31, 32, 33, 34, 35],
+                         create_one_continuous_grid_quorum(36, 5, 5))
