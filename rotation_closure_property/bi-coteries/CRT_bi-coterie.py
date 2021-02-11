3cd1a13857e575bc112c688613c5c477eb400594
109598087 <t109598087@ntut.org.tw>
2021-02-11 20:31:25 +0800

    refactory CRT_bi-coterie

--- rotation_closure_property/bi-coteries/CRT_bi-coterie.py
+++ rotation_closure_property/bi-coteries/CRT_bi-coterie.py
@@ -1,6 +1,15 @@
 # cluster head => p1*k1(mod N)| 0 <= k1 <=N/p1-1 U p2*k2(mod N)| 0 <= k2 <=N/p2-1
 # cluster member => p3*k3(mod N)| 0 <= k3 <=N/p3-1
 from rotation_closure_property.is_rotation_closure_property import is_rotation_closure_property
+from same_function import create_one_crt_quorum
+
+
+def create_CRT_bi_coteries_clustered_head_quorum(p1, p2, N):
+    return create_one_crt_quorum(p1, N) + create_one_crt_quorum(p2, N)
+
+
+def create_CRT_bi_coteries_clustered_member_quorum(p, N):
+    return create_one_crt_quorum(p, N)
 
 
 def create_CRT_bi_coteries_clustered_head_and_member_quorum(list_of_three_p):
@@ -8,28 +17,21 @@ def create_CRT_bi_coteries_clustered_head_and_member_quorum(list_of_three_p):
     N = 1  # 初始化
     for p in list_of_three_p:
         N *= p
-
-    CRT_bi_coteries_clustered_head_quorum = list()
     p1 = list_of_three_p[0]
     p2 = list_of_three_p[1]
-    k1 = int(N / p1 - 1)
-    k2 = int(N / p2 - 1)
-    for i in range(0, k1 + 1):
-        CRT_bi_coteries_clustered_head_quorum.append(p1 * i % N)
-    for i in range(0, k2 + 1):
-        CRT_bi_coteries_clustered_head_quorum.append(p2 * i % N)
-
+    CRT_bi_coteries_clustered_head_quorum = create_CRT_bi_coteries_clustered_head_quorum(p1, p2, N)
     p3 = list_of_three_p[2]
-    k3 = int(N / p3 - 1)
-    CRT_bi_coteries_clustered_member_quorum = [p3 * i % N for i in range(0, k3 + 1)]
-    return list(set(CRT_bi_coteries_clustered_head_quorum)), list(
-        set(CRT_bi_coteries_clustered_head_quorum)), CRT_bi_coteries_clustered_member_quorum
+    CRT_bi_coteries_clustered_member_quorum = create_CRT_bi_coteries_clustered_member_quorum(p3, N)
+    return [set(CRT_bi_coteries_clustered_head_quorum),
+            set(CRT_bi_coteries_clustered_head_quorum),
+            CRT_bi_coteries_clustered_member_quorum]
 
 
 p_list = [2, 3, 5]
 N = 2 * 3 * 5
 CRT_bi_coteries_clustered_head_and_member_quorum_system = create_CRT_bi_coteries_clustered_head_and_member_quorum(
     p_list)
+print(CRT_bi_coteries_clustered_head_and_member_quorum_system)
 print(len(CRT_bi_coteries_clustered_head_and_member_quorum_system[0]),
       len(CRT_bi_coteries_clustered_head_and_member_quorum_system[1]),
       len(CRT_bi_coteries_clustered_head_and_member_quorum_system[2]))
